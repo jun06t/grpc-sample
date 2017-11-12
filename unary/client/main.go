22c20808c8a7fff9721d306c04ce417cd2399ed7
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2017-11-13 07:18:08 +0900

    Added grpc client

---
+++ unary/client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/jun06t/grpc-sample/unary/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:8080"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+
+	req := &pb.HelloRequest{
+		Name: "alice",
+	}
+	resp, err := c.SayHello(context.Background(), req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Reply: ", resp.Message)
+}
