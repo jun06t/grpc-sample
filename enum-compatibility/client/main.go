69aa1eb357928c42c387c5005e0b7a57b0bf47c1
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2020-01-14 22:18:50 +0900

    Added enum compatibility sample

---
+++ enum-compatibility/client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/jun06t/grpc-sample/enum-compatibility/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:8080"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+
+	req := &pb.HelloRequest{
+		Name: "alice",
+	}
+	resp, err := c.SayHello(context.Background(), req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Reply: ", resp.Message)
+	log.Println("Role: ", resp.Role.String())
+}
---
+++ enum-compatibility/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/jun06t/grpc-sample/enum-compatibility/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{
+		Message: "Hello " + in.Name,
+		Role:    pb.Role_ULTIMATE,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
